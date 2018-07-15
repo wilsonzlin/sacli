a63dacd053cce22c71fb0d386d979d0b023a4c20
Wilson Lin <contact@wilsonl.in>
2018-07-16 02:04:36 +1200

    Export/declare module functions and types

---
+++ src/main/ts/main.ts
@@ -0,0 +1,6 @@
+export {default as Option} from "./opt/Option";
+export {default as Command} from "./cmd/Command";
+export {default as CLI} from "./cli/CLI";
+
+export {default as parse} from "./parse/parse";
+export {default as build} from "./build/build";
